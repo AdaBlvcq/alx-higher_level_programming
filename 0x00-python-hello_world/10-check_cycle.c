a3c106ec1d265d0d6815f716e3dd5637a54540e9
Nana Afia <nnafya@gmail.com>
2023-05-08 13:08:43 -0700

    Linked List

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure for Betty style compliance
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+/**
+ * check_cycle - Checks if a singly linked list has a cycle in it.
+ * @list: Pointer to the head of the linked list.
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle.
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow, *fast;
+
+	if (list == NULL)
+		return (0);
+
+	slow = list;
+	fast = list->next;
+
+	while (fast && fast->next)
+	{
+		if (slow == fast)
+			return (1);
+
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+
+	return (0);
+}
+
