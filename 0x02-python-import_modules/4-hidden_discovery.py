1186d5b48c07afa27cf8d1e91461ee89691792b5
Nana Afia <nnafya@gmail.com>
2023-05-11 08:18:31 -0700

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    import hidden_4
-    for i in dir(hidden_4):
-        if i[:2] != "__":
-            print("{:i}".format(i))
+def magic_calculation(a, b):
+
+    from magic_calculation_102 import add, sub
+
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return (c)
+
+    else:
+        return(sub(a, b))
